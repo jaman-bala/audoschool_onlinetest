456c63cedc99c43074a087d2c5e78ed26f6507de
jaman-bala <ermekoffdastan@yandex.ru>
2024-12-12 12:29:50 +0600

    up:08 version docker db

--- src/main.py
+++ src/main.py
@@ -54,4 +54,4 @@ app.include_router(router_totals)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
+    uvicorn.run("src.main:app", reload=True, host="0.0.0.0", port=8000)
